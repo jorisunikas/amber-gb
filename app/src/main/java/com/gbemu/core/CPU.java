bfe86d4209cc17223bf2ee5c970612e5343c3542
Joris Unikas <joris.unikas21@gmail.com>
2026-03-21 21:16:26 +0200

    added opcode management and opcodes for 00, 18, c3 instructions

--- app/src/main/java/com/gbemu/core/CPU.java
+++ app/src/main/java/com/gbemu/core/CPU.java
@@ -3,21 +3,27 @@ package com.gbemu.core;
 public class CPU {
     private final Registers registers;
     private final MMU mmu;
+    private int cycles;
+    private final Runnable[] opcodes;
 
     public CPU(MMU mmu) {
-        registers = new Registers();
         this.mmu = mmu;
+
+        registers = new Registers();
+        opcodes = new Runnable[256];
+
+        cycles = 0;
+        initOpcodes();
     }
 
     public int step() {
         int opcode = fetch();
         int cycles = 0;
-        switch (opcode) {
-            case 0x00 -> cycles = 4; // NOP
-            default -> System.out.println("Unimplemented opcode: 0x" +
-                    Integer.toHexString(opcode));
+        Runnable handler = opcodes[opcode];
+        if (handler == null)
+            System.out.println("Unrecognised opcode: 0x" + Integer.toHexString(opcode));
 
-        }
+        handler.run();
         return cycles;
     }
 
@@ -25,5 +31,30 @@ public class CPU {
         int opcode = mmu.readByte(registers.pc);
         registers.incPC();
         return opcode;
+    }
+
+    private void initOpcodes() {
+        opcodes[0x00] = () -> cycles = 4;
+        opcodes[0x18] = () -> {
+            jr_s8();
+            cycles = 12;
+        };
+        opcodes[0xC3] = () -> {
+            jp_u16();
+            cycles = 16;
+        };
+    }
+
+    // u(nsigned) vs s(igned)
+
+    private void jp_u16() {
+        int high = fetch();
+        int low = fetch();
+        registers.setSP(high << 8 | low);
+    }
+
+    private void jr_s8() {
+        int offset = (byte) fetch();
+        registers.setSP(registers.getSP() + offset);
     }
 }
