9770f65303b442e7bcc0f301180c46d87eb273e1
Joris Unikas <joris.unikas21@gmail.com>
2026-03-21 21:16:25 +0200

    added callback on serial write in order to successfully test if ROM tests pass

--- app/src/main/java/com/gbemu/core/MMU.java
+++ app/src/main/java/com/gbemu/core/MMU.java
@@ -2,12 +2,15 @@ package com.gbemu.core;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.Consumer;
 
 public class MMU {
     private final int[] memory;
+    private Consumer<Integer> serialCallback;
 
     public MMU() {
         memory = new int[0x10000];
+        serialCallback = null;
     }
 
     public int readByte(int adress) {
@@ -15,13 +18,18 @@ public class MMU {
     }
 
     public void writeByte(int adress, int value) {
-        memory[adress & 0xFFFF] = (value & 0xFF);
+        adress = adress & 0xFFFF;
+        value = value & 0xFF;
+
+        checkSerialCallback(adress, value);
+
+        memory[adress] = value;
     }
 
     public void loadROM(String path) {
         byte[] rom = null;
         try {
-             rom = Files.readAllBytes(Path.of(path));
+            rom = Files.readAllBytes(Path.of(path));
         } catch (Exception e) {
             System.out.println(e.toString());
             return;
@@ -29,5 +37,15 @@ public class MMU {
         for (int i = 0; i < Math.min(rom.length, 0x8000); i++) {
             memory[i] = rom[i];
         }
+    }
+
+    public void setSerialCallback(Consumer<Integer> callback) {
+        this.serialCallback = callback;
+    }
+
+    private void checkSerialCallback(int adress, int value) {
+        if (adress == 0xFF02 && value == 0x81 && serialCallback != null)
+            serialCallback.accept(memory[0xFF01]);
+
     }
 }
