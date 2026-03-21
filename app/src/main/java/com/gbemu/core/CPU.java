126ff7c024562e8e5fecd57fcbd4e23f36238a74
Joris Unikas <joris.unikas21@gmail.com>
2026-03-21 21:16:25 +0200

    Decoupled mmu from cpu

--- app/src/main/java/com/gbemu/core/CPU.java
+++ app/src/main/java/com/gbemu/core/CPU.java
@@ -1,5 +1,27 @@
 package com.gbemu.core;
 
 public class CPU {
-    
+    private final Registers registers;
+    private final MMU mmu;
+
+    public CPU(MMU mmu) {
+        registers = new Registers();
+        this.mmu = mmu;
+    }
+
+    public void step() {
+        int opcode = fetch();
+        excecute(opcode);
+    }
+
+    private int fetch() {
+        int opcode = mmu.readByte(registers.pc);
+        registers.incPC();
+        return opcode;
+    }
+
+    private void excecute(int opcode) {
+        switch (opcode) {
+        }
+    }
 }
