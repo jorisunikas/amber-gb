4085384c8a085edd3c59e754f2ade9c0899b5fb1
Joris Unikas <joris.unikas21@gmail.com>
2026-04-04 18:14:14 +0300

    add timer to main loop

--- app/src/main/java/com/gbemu/core/Console.java
+++ app/src/main/java/com/gbemu/core/Console.java
@@ -1,16 +1,19 @@
 package com.gbemu.core;
 
 import com.gbemu.display.Display;
+import com.gbemu.core.timer.Timer;
 
 public class Console {
     private Display display;
     private CPU cpu;
     private MMU mmu;
+    private Timer timer;
 
     public Console() {
         this.display = new Display();
         this.mmu = new MMU();
         this.cpu = new CPU(this.mmu);
+        this.timer = new Timer(this.mmu);
     }
 
     public Display getDisplay() {
@@ -20,6 +23,7 @@ public class Console {
     public void run(){
         while (true) {
             int cycles = cpu.step();
+            timer.step(cycles);
         }
     }
 }
