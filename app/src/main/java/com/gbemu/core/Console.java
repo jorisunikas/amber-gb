44c4f9c69ff2aef63e2af1d5b69bd607ab33da49
Joris Unikas <joris.unikas21@gmail.com>
2026-04-06 18:02:11 +0300

    make gameboy's frame last 16,74ms; load test ppu ROM; make ppu sync with cpu and send image to display

--- app/src/main/java/com/gbemu/core/Console.java
+++ app/src/main/java/com/gbemu/core/Console.java
@@ -2,7 +2,11 @@ package com.gbemu.core;
 
 import com.gbemu.display.Display;
 import com.gbemu.core.timer.Timer;
+
+import java.nio.file.Path;
+
 import com.gbemu.core.cpu.CPU;
+import com.gbemu.core.graphics.PPU;
 import com.gbemu.core.memory.MMU;
 
 public class Console {
@@ -10,22 +14,45 @@ public class Console {
     private CPU cpu;
     private MMU mmu;
     private Timer timer;
+    private PPU ppu;
+
+    private static final long FRAME_DURATION_NS = 16_742_706L;
 
     public Console() {
         this.display = new Display();
         this.mmu = new MMU();
         this.cpu = new CPU(this.mmu);
         this.timer = new Timer(this.mmu);
+        this.ppu = new PPU(mmu);
+        mmu.loadROM(Path.of("src/main/resources/dmg-acid2.gb"));
     }
 
     public Display getDisplay() {
         return this.display;
     }
 
-    public void run(){
+    public void run() {
+        long nextFrameTime = System.nanoTime();
+
         while (true) {
-            int cycles = cpu.step();
-            timer.step(cycles);
+            int cyclesThisFrame = 0;
+            while (cyclesThisFrame < 70224) {
+                int cycles = cpu.step();
+                timer.step(cycles);
+                ppu.step(cycles);
+                cyclesThisFrame += cycles;
+            }
+
+            display.render(ppu.getFramebuffer());
+            nextFrameTime += FRAME_DURATION_NS;
+            long sleepNs = nextFrameTime - System.nanoTime();
+            if(sleepNs > 0){
+                try {
+                    Thread.sleep(sleepNs / 1_000_000, (int) (sleepNs % 1_000_000));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
     }
 }
