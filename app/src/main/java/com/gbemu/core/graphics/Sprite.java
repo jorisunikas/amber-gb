7951b7796e7d93e5213dea1a4d5a5f9605ae3447
Joris Unikas <joris.unikas21@gmail.com>
2026-04-07 12:41:08 +0300

    Sprite class for easier data management

---
+++ app/src/main/java/com/gbemu/core/graphics/Sprite.java
@@ -0,0 +1,29 @@
+package com.gbemu.core.graphics;
+
+public class Sprite {
+    public final int y, x, tileIndex, attributes;
+
+    public Sprite(int y, int x, int tileIndex, int attributes) {
+        this.y = y;
+        this.x = x;
+        this.tileIndex = tileIndex;
+        this.attributes = attributes;
+    }
+
+    /** {return} opposite of bit7 in attributes */
+    public boolean hasPriority() {
+        return ((attributes >> 7) & 0x01) == 1;
+    }
+
+    public boolean isFlipX() {
+        return ((attributes >> 5) & 0x01) == 1;
+    }
+
+    public boolean isFlipY() {
+        return ((attributes >> 6) & 0x01) == 1;
+    }
+
+    public int getPalette() {
+        return ((attributes >> 4) & 0x01);
+    }
+}
